7f6ace843bba952a815bd22b2a264ae8ab2e7d83
Sabolc Jut <sab.jut@googlemail.com>
2022-11-26 14:01:45 +0100

    fix eval_ens

--- eval_ens.sh
+++ eval_ens.sh
@@ -12,8 +12,7 @@ echo using $DEVICE
 python3 val.py \
     --data data/data_bothdn.yaml \
     --batch-size 1 \
-    --weights best_d.pt \
-    --weights2 best_n.pt \
+    --weights best_d.pt best_n.pt \
     --task test \
     --name "ensemble" \
     --device $DEVICE
